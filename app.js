ed3cd4bd8e16d59adaa94a7189e86cb65cd8df75
PatriciaRivas1324 <91750744+PatriciaRivas1324@users.noreply.github.com>
2025-05-28 22:43:24 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const mensaje = document.createElement("p");
+  mensaje.textContent = "El archivo app.js se ejecutó correctamente.";
+  document.body.appendChild(mensaje);
+});
+
+
---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'mi-pwa-v1';
+const RUTAS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/offline.html',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(RUTAS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request).catch(() => caches.match('/offline.html'))
+    )
+  );
+});
